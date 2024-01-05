2c7ec2d2a3edc30ff944b4fc8bdf66d77897f189
Ian Vigilante <ian.vigilante@innovid.com>
2024-01-05 17:05:45 -0300

    .

--- Assets/Scripts/MoveAnt.cs
+++ Assets/Scripts/MoveAnt.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveAnt : MonoBehaviour
-{   
+{
     public float maxSpeed = 3.5f;
     public float steerStrength = 6f;
     public float wanderStrength = 8f;
@@ -19,30 +19,35 @@ public class MoveAnt : MonoBehaviour
 
     public GameObject target;
 
-    RaycastHit2D checkCollisionInWanderDirection(){
+    RaycastHit2D checkCollisionInWanderDirection()
+    {
         return Physics2D.Raycast(position, desiredDirection, maxSpeed, obstacleMask);
 
     }
     Vector2 WanderDirection()
-    {   
+    {
         desiredDirection = (desiredDirection + UnityEngine.Random.insideUnitCircle).normalized * wanderStrength;
         if (checkCollisionInWanderDirection()) desiredDirection *= -1;
         return desiredDirection;
     }
 
     void Update()
-    {   
+    {
         position = transform.position;
 
         if (target)
             desiredDirection = ((Vector2)target.transform.position - (Vector2)transform.position).normalized;
+        if (checkCollisionInWanderDirection())
+        {
+            desiredDirection *= -1;
+        }
         else
             desiredDirection = WanderDirection();
 
 
         Vector2 desiredVelocity = desiredDirection * maxSpeed;
         Vector2 desiredSteeringForce = (desiredVelocity - velocity) * steerStrength;
-        Vector2 acceleration = Vector2.ClampMagnitude(desiredSteeringForce, steerStrength)/1;
+        Vector2 acceleration = Vector2.ClampMagnitude(desiredSteeringForce, steerStrength) / 1;
 
         velocity = Vector2.ClampMagnitude(velocity + acceleration * Time.deltaTime, maxSpeed);
         position += velocity * Time.deltaTime;
@@ -51,7 +56,7 @@ public class MoveAnt : MonoBehaviour
         currentForwardDir = velocity.normalized;
         transform.SetPositionAndRotation(new Vector3(position.x, position.y, -0.1f), Quaternion.FromToRotation(-Vector3.up, -currentForwardDir));
 
-        
-        
+
+
     }
 }
